66da1b1b18fb1547c1f31952b2358d89a15b9be7
hypnosw <hypnoswu@gmail.com>
2023-10-18 21:16:10 -0700

    changeed app.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
       <HashRouter>
           <Routes>
-              {/*<Route path="/"         element={<Navigate to="/Labs"/>}/>*/}
+              <Route path="/"         element={<Navigate to="/Labs"/>}/>
               <Route path="/hello"    element={<HelloWorld/>}/>
-              <Route index element={<Labs/>}/>
+              <Route path="/labs" element={<Labs/>}/>
               <Route path="/Kanbas/*" element={<Kanbas/>}/>
           </Routes>
       </HashRouter>
