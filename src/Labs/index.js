d170a6839022c5b170fc9739e26f68f64cca9ce9
hypnosw <hypnoswu@gmail.com>
2023-10-23 18:56:26 -0700

    added a4 to nav bar functioning as expected

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Assignment3 from "./a3";
 import {Link} from "react-router-dom";
 import Nav from "../Nav";
+import Assignment4 from "./a4/index";
+import {Route, Routes} from "react-router";
 
 function Labs() {
     return(
         <div>
             <Nav/>
-            <Assignment3/>
+            <Routes>
+                <Route path="/a3" element={<Assignment3/>}></Route>
+                <Route path="/a4" element={<Assignment4/>}></Route>
+            </Routes>
         </div>
     );
 }
