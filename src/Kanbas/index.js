4a024f5d151b0e65527da18edd2a8ae692e93604
hypnosw <hypnoswu@gmail.com>
2023-11-08 18:58:07 -0800

    starting 4.3

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -87,7 +87,7 @@ function Kanbas() {
                             addNewCourse={addNewCourse}
                             deleteCourse={deleteCourse}
                             updateCourse={updateCourse}/>} />
-                        <Route path="Courses/:courseId/*" element={<Courses courses={courses}/>} />
+                        <Route path="Courses/:courseId/*" element={<Courses/>} />
 
                     </Routes>
                 </div>
