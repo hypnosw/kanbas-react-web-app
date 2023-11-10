e961125d020cee485eaaefa4e455079ec7515697
hypnosw <hypnoswu@gmail.com>
2023-11-09 16:38:40 -0800

    fixing assignment post and put

--- src/Kanbas/Courses/Assignments/client.js
+++ src/Kanbas/Courses/Assignments/client.js
@@ -9,3 +9,12 @@ export const deleteDBAssignment = async(id)=>{
     const response = await axios.delete(`${ASSIGNMENTS_URL}/${id}`);
     return response.data;
 }
+
+export const putAssignment = async (id, assignment)=>{
+    const response = await axios.put(`${ASSIGNMENTS_URL}/${id}`, assignment);
+    return response.data;
+}
+export const postAssignment = async(assignment)=>{
+    const response = await axios.post(`${ASSIGNMENTS_URL}/`, assignment);
+    return response.data;
+}
